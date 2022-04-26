9f950958132ad5ae61c7cc497e6d48e3a3940d1a
viper-vlad <vlad.052898@gmail.com>
2022-04-27 00:13:03 +0600

    build: no-param-reassign in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'lines-between-class-members': 'off',
     'class-methods-use-this': 'off',
     'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
     camelcase: 'off',
     'no-shadow': 'off',
     'import/extensions': [
