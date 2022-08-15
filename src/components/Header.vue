393d45619be825aeeb0269343e5b6baec28bc60b
Shakarim <mukhamediyev71636@gmail.com>
2022-08-15 20:06:22 +0600

    half of nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,16 +29,76 @@
         </div>
       </div>
       <div class="header__menu menu">
-        <div class="header__nav nav">
+        <div class="header__nav nav"
+             :class="{ 'nav--opened': showMobileMenu }"
+        >
+          <div class="header__city-selector header__city-selector--nav city-selector">
+            <div class="city-selector__current">
+              Город
+              <i class="ac-icon ac-icon-arrow-down city-selector__current-arrow"></i>
+            </div>
+            <div class="city-selector__items">
+              <a
+                v-for="city in citiesList" :key="city"
+                href="#"
+                @click.prevent="changeCity(city)"
+                class="link city-selector__item"
+                :class="{ 'city-selector__item--active': city.slug === locationCity.slug }">
+                {{ city.name }}
+              </a>
+            </div>
+          </div>
+          <router-link custom to="/home"
+                       v-slot="{ navigate }">
+            <a href="#"
+               @click="navigate"
+               class="link link--gray menu__item nav__item nav__item--home">Главная</a>
+          </router-link>
           <router-link custom to="/about"
                        v-slot="{ navigate }">
             <a href="#"
                @click="navigate" class="link link--gray menu__item nav__item">Компания</a>
           </router-link>
           <a href="#faq" class="link link--gray menu__item nav__item">Частые вопросы</a>
-          <a href="#" class="link link--gray menu__item nav__item">Приложение</a>
+          <a href="#" class="link link--gray menu__item nav__item nav__item--app">Приложение</a>
+          <div v-if="profile !== null"
+               class="user-bar"
+               :class="{
+                'user-bar--loading': logoutLoading,
+             }">
+            <div class="user-bar__user-name">
+              <div class="user-bar__user-name-text">
+                {{ profile.firstName }} {{ profile.lastName.substr(0, 1) }}.
+              </div>
+              <i class="ac-icon ac-icon-arrow-down user-bar__user-name-arrow"></i>
+              <div class="user-bar__loader">
+                <LoaderHor/>
+              </div>
+            </div>
+            <div class="user-bar__links">
+              <router-link custom to="/profile"
+                           v-slot="{ navigate }">
+                <a href="#" @click="navigate" class="link user-bar__link">Личный кабинет</a>
+              </router-link>
+              <div class="user-bar__link-delimiter"></div>
+              <a href="#"
+                 @click.prevent="logout"
+                 class="link user-bar__link">Выйти</a>
+            </div>
+          </div>
+          <div v-else-if="profileLoading" class="header__user-loading">
+            <Loader color="grey"/>
+          </div>
+          <router-link v-else custom to="/auth/sign-up"
+                       v-slot="{ navigate }">
+            <a href="#" @click="navigate" class="btn btn--small btn--primary
+          btn--text-medium btn--circle nav__item nav__item--user">Личный кабинет</a>
+          </router-link>
         </div>
-        <div class="nav-btn">
+        <div class="nav-btn"
+             :class="{ 'nav-btn--active': showMobileMenu }"
+             @click="showMobileMenu = !showMobileMenu"
+        >
           <div class="nav-btn__line"></div>
           <div class="nav-btn__line"></div>
           <div class="nav-btn__line"></div>
@@ -85,7 +145,6 @@
           btn--text-medium btn--circle">Личный кабинет</a>
         </router-link>
       </div>
-
     </div>
   </header>
   <!--END HEADER-->
@@ -116,6 +175,7 @@ export default class Header extends Vue {
   readonly locationCity!: CityInformation;
   readonly citiesList: CityInformation[] = citiesList;
   logoutLoading = false;
+  showMobileMenu = false;
 
   changeCity(city: CityInformation): void {
     this.$store.dispatch('setLocationCity', city.slug);
